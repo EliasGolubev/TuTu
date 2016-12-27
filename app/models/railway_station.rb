f136bc5b8101c82db5f77fdff9245ebac8cc6334
Elias <igolubevssmnu@mail.ru>
2016-12-27 22:25:39 +0300

    [Railway Station] Fixed associative

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,5 +1,4 @@
 class RailwayStation < ApplicationRecord
   has_and_belongs_to_many :routes
   has_many :trains, foreign_key: :current_station_id
-  has_many :tickets, foreign_key: :first_station_id
 end
