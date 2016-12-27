40460052393f44343d6f4659c2ba3bee5a0e21d2
Elias <igolubevssmnu@mail.ru>
2016-12-27 21:34:59 +0300

    [RailwayStation model] Create asociative Tickets first station, and fixed trains

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,4 +1,5 @@
 class RailwayStation < ApplicationRecord
   has_and_belongs_to_many :routes
-  has_many :trains
+  has_many :trains, foreign_key: :current_station_id
+  has_many :tickets, foreign_key: :first_station_id
 end
