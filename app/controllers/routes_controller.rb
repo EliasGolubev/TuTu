03f8d0e20559e679ec834e1e702d59564de95fd0
Elias <igolubevssmnu@mail.ru>
2017-01-14 14:43:21 +0300

    Delete comments

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,65 +1,45 @@
 class RoutesController < ApplicationController
   before_action :set_route, only: [:show, :edit, :update, :destroy]
-  #
-  # Получает все объекты класса Route
-  #
+
   def index
     @routes = Route.all
   end
-  #
-  # Загружает конкретный объект
-  #
+
   def show
   end
-  #
-  # Создает новый объект Route
-  #
+
   def new
     @route = Route.new
   end
 
-  #
-  # Создает новый объект Route
-  #
   def create
     @route = Route.new(route_params)
-    # Проверка валидации
+
     if @route.save
       redirect_to @route
     else
       render :new
     end
   end
-  #
-  # Загружает объект Route
-  # Рендерит этот объект
-  #
+
   def edit  
   end
-  #
-  # Загружает из базы объект
-  # И обновляет объект в базе данных если прошла валидация.
-  #
+
   def update    
-    # Проверка валидации
     if @route.update(route_params)
       redirect_to @route
     else
       render :edit
     end
   end
-  #
-  # Удаляет объект из базы данных
-  #
+
   def destroy
     @route.destroy
     redirect_to routes_path
   end
 
   private
-  #
-  # Strong params 
-  #
+
   def route_params
     params.require(:route).permit(:name)
   end
