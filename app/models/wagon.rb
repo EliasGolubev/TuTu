e3b3ad2d1c50bccfe2ac3df9fb2d038eb8ee72d7
Elias <igolubevssmnu@mail.ru>
2017-01-21 16:09:28 +0300

    Create automatic coun number wagon

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,3 +1,13 @@
 class Wagon < ApplicationRecord
   belongs_to :train
+  
+
+  validates :number, presence: { message: 'номер не назначен' }
+  before_validation :count_number
+
+  protected
+
+  def count_number
+    self.number = Wagon.where(train_id: self.train).maximum('number').to_i + 1
+  end
 end
