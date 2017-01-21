c8400ed414b351d9f1e26f2e2160a6ac9bbe803f
Elias <igolubevssmnu@mail.ru>
2017-01-21 17:53:29 +0300

    Validate wagon number uniq

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -3,25 +3,13 @@ class Wagon < ApplicationRecord
   
 
   validates :number, presence: { message: 'номер не назначен' }
+  validates :number, uniqueness: { scope: :train_id }
   
   before_validation :count_number
 
   scope :at_first, -> { order('number ASC') }
   scope :from_end, -> { order('number DESC') }
 
-  scope :sum_coupe_top_seats,    -> { where(type: CoupeWagon).sum('top_seats') }
-  scope :sum_coupe_bottom_seats, -> { where(type: CoupeWagon).sum('bottom_seats') }
-
-  scope :sum_economy_top_seats,         -> { where(type: EconomyWagon).sum('top_seats') }
-  scope :sum_economy_bottom_seats,      -> { where(type: EconomyWagon).sum('bottom_seats') }
-  scope :sum_economy_side_top_seats,    -> { where(type: EconomyWagon).sum('side_top_seats') }
-  scope :sum_economy_side_bottom_seats, -> { where(type: EconomyWagon).sum('side_bottom_seats') }
-
-  scope :sum_sv_bottom_seats,      -> { where(type: SVWagon).sum('bottom_seats') }
-  scope :sum_sv_side_bottom_seats, -> { where(type: SVWagon).sum('side_bottom_seats') }
-
-  scope :sum_sitting_seats, -> { where(type: SittingWagon).sum('sitting_seats') }
-  
   protected
 
   def count_number
